2b485ca69d6a3510f887ccb4d32c78e49afad10e
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-21 10:14:15 +0600

    first commit of a react project

---
+++ src/components/Device/Device.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DeviceDetail from '../DeviceDetal/DeviceDetal';
+
+
+const Device = (props) => {
+    return (
+        <div>
+            <h2>I have:{props.name}</h2>
+            <DeviceDetail price={props.price}></DeviceDetail>
+        </div>
+    );
+};
+
+export default Device;
